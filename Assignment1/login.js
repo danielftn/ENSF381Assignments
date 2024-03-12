a5065b649ca3e25dfb21891353fbeaad3fc24f0c
Ayodeji <danielfatokun@gmail.com>
2024-03-11 20:41:55 -0600

    updated login

--- Assignment1/login.js
+++ Assignment1/login.js
@@ -1,30 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("loginForm");
-    const messageBox = document.getElementById("messageBox");
-  
-    loginForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
-  
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("API call unsuccessful");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          const userExists = data.some((user) => user.username === username);
-  
-          const messageText = document.createElement("p");
-          messageText.textContent = userExists ?"Login successful" : "Invalid username";
-          messageBox.innerHTML = "";
-          messageBox.appendChild(messageText);
-        })
-        .catch((error) => {
-          alert("API call unsuccessful");
-        });
-    });
+  const loginForm = document.getElementById("loginForm");
+  const messageBox = document.getElementById("messageBox");
+
+  loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+
+      const users = await response.json();
+      const userExists = users.some((user) => user.username === username);
+      
+      const messageText = document.createElement("p");
+      messageText.textContent = userExists ?"Login successful" : "Invalid username";
+      messageBox.innerHTML = "";
+      messageBox.appendChild(messageText);
+
+    } catch (error) {
+      alert("Failed to fetch user data");
+    }
   });
-  
+}); 
